0f96f77fdd04f42199d033423faf30a81e0c6f0e
Luis Ruiz <la.ruiz967@uniandes.edu.co>
2017-10-27 17:47:07 -0500

    Ultimas modificaciones a la página, cambio de imagenes

---
+++ landing/js/index_v3.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function(){
+
+    var myIndex = 0;
+    var myIndexHome = 0;
+    carousel();
+    carouselHome();
+
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+          x[i].style.display = "none";
+        }
+        myIndex++;
+        if (myIndex > x.length) {myIndex = 1}
+        x[myIndex-1].style.display = "block";
+        setTimeout(carousel, 4000);
+    }
+
+    function carouselHome() {
+        var i;
+        var x = document.getElementsByClassName("mySlidesHome");
+        for (i = 0; i < x.length; i++) {
+          x[i].style.display = "none";
+        }
+        myIndexHome++;
+        if (myIndexHome > x.length) {myIndexHome = 1}
+        x[myIndexHome-1].style.display = "block";
+        setTimeout(carouselHome, 4000);
+    }
+});
